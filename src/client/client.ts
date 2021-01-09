8b22cb9b194fb5d5d62600ac6163f384f56af375
Richard Breslin <18ribreslin@gmail.com>
2021-01-08 19:00:36 -0500

    gitignore & comments

--- src/client/client.ts
+++ src/client/client.ts
@@ -8,22 +8,26 @@ const scene: THREE.Scene = new THREE.Scene()
 //init camera class
 const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
-//webgl renderer
+//init webgl renderer
 const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
+//init control module
 const controls = new OrbitControls(camera, renderer.domElement)
 
-
+//create a simple wireframe box
 const geometry: THREE.BoxGeometry = new THREE.BoxGeometry()
 const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
 
+// add box to scene
 const cube: THREE.Mesh = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
+//init camera position
 camera.position.z = 2;
 
+//event listener updates the canvas when the window is resized
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight
@@ -32,6 +36,7 @@ function onWindowResize() {
     renderer.render(scene, camera)
 }
 
+//stats window in top left - FPS counter, RAM etc.
 const stats = Stats()
 document.body.appendChild(stats.dom)
 
@@ -39,8 +44,8 @@ document.body.appendChild(stats.dom)
 var animate = function () {
     requestAnimationFrame(animate)
 
-    cube.rotation.x += 0.001;
-    cube.rotation.y += 0.001;
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
 
     controls.update()
 
